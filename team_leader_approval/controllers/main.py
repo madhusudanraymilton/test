bb263e91180ae4376449dd0d2c8d45a72d4eab4a
madhusudanraymilton <madhusudanraymilton@gmail.com>
2025-12-06 15:20:33 +0600

    update

--- team_leader_approval/controllers/main.py
+++ team_leader_approval/controllers/main.py
@@ -1,10 +1,20 @@
 from odoo import http
 from odoo.http import request
 
+
 class TeamLeaderApprovalController(http.Controller):
-    @http.route('/team_leader/approve', type='http', auth='user', website=True)
-    def approve_request(self, request_id):
-       for rec in self:
-           return request.render('team_leader_approval.portal_team_leader_approvals', {
-               'request_id': request_id,
-           })
+
+    # Main page to display all leave approvals
+    @http.route('/my/leave/team-leader/approvals', type='http', auth='user', website=True)
+    def team_leader_approvals_page(self, **kw):
+        """Display the team leader approval page with all necessary data"""
+
+        # Get current user's employee record
+        user = request.env.user
+        employee = request.env['hr.employee'].sudo().search([
+            ('user_id', '=', user.id)
+        ], limit=1)
+
+        return request.render('team_leader_approval.portal_team_leader_approvals', {
+            'user': user,
+        })
--- team_leader_approval/models/team_leader.py
+++ team_leader_approval/models/team_leader.py
@@ -1,5 +1,59 @@
 from odoo import api, fields, models, exceptions
 
-class TeamLeader(models.Model):
+
+class HrLeaveInherit(models.Model):
+    _inherit = 'hr.leave'
+
+    team_leader_approved = fields.Boolean(
+        string='Team Leader Approved',
+        default=False,
+        tracking=True,
+    )
+
+    team_leader_approval_date = fields.Datetime(
+        string='Team Leader Approval Date',
+        readonly=True,
+    )
+
+    @api.model
+    def create(self, vals):
+        """Override create to set initial state for team leader approval"""
+        leave = super(HrLeaveInherit, self).create(vals)
+
+        # If employee has a team leader (parent_id), set state to team_leader_approval
+        if leave.employee_id.parent_id:
+            leave.state = 'team_leader_approval'
+
+        return leave
+
+    def write(self, vals):
+        """Override write to track team leader approval"""
+        if vals.get('team_leader_approved'):
+            vals['team_leader_approval_date'] = fields.Datetime.now()
+
+        return super(HrLeaveInherit, self).write(vals)
+
+    def action_approve(self):
+        """Override approval to check team leader approval first"""
+        for leave in self:
+            if leave.employee_id.parent_id and not leave.team_leader_approved:
+                raise exceptions.UserError(
+                    'This leave request must be approved by the team leader first.'
+                )
+        return super(HrLeaveInherit, self).action_approve()
+
+
+class HrEmployee(models.Model):
     _inherit = 'hr.employee'
 
+    is_team_leader = fields.Boolean(
+        string='Is Team Leader',
+        compute='_compute_is_team_leader',
+        store=True,
+    )
+
+    @api.depends('child_ids')
+    def _compute_is_team_leader(self):
+        """Compute if employee is a team leader"""
+        for employee in self:
+            employee.is_team_leader = bool(employee.child_ids)
